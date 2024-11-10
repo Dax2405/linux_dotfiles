be9865dada986ab5b561c45f45ddcb03a69f8a33
Dax <daxnavarrete@gmail.com>
2024-11-09 21:03:35 -0500

    remove txt regex in add_hack_nerd_fonts.bash

--- common/add_hack_nerd_fonts.bash
+++ common/add_hack_nerd_fonts.bash
@@ -3,6 +3,6 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
 mkdir Hack_Nerd_Fonts
 unzip Hack.zip -d Hack_Nerd_Fonts
 cd Hack_Nerd_Fonts
-rm *.md *.txt
+rm *.md
 cd ..
 sudo mv Hack_Nerd_Fonts /usr/share/fonts/
