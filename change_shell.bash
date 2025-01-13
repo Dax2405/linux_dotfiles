776dd5be614efa7a6bf3fc43bc0898d3d9f163cf
Dax <daxnavarrete@gmail.com>
2025-01-13 15:05:21 -0500

    feat: add vscode config

--- change_shell.bash
+++ change_shell.bash
@@ -1,6 +1,6 @@
 
 echo -e "\e[32mChanging shell to zsh\e[0m"
-sudo chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
